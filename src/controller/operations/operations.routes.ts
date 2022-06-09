a32a08461a756307f1258802b58031ed9d335ca7
Arnil Duyaguit <neil.gd@gmail.com>
2022-06-09 20:03:29 +0800

    Add validation for multiplication (#2)

--- src/controller/operations/operations.routes.ts
+++ src/controller/operations/operations.routes.ts
@@ -5,8 +5,13 @@ import operationHandler from '@controller/operations/operations.handler';
 const route = Router();
 
 route.post('/addition', ValidateReq('body', 'AdditionOperation'), operationHandler.addition);
+<<<<<<< HEAD
+route.post('/subtraction', operationHandler.subtraction);
+route.post('/multiplication', ValidateReq('body', 'MultiplicationOperation'), operationHandler.multiplication);
+=======
 route.post('/subtraction', ValidateReq('body', 'SubtractionOperation'), operationHandler.subtraction);
 route.post('/multiplication', operationHandler.multiplication);
+>>>>>>> master
 route.post('/division', operationHandler.division);
 
 export default route;
