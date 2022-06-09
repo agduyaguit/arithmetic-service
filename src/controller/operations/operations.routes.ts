781e1de991cae43ac71227206b09d576abf478d3
Arnil Duyaguit <neil.gd@gmail.com>
2022-06-09 19:47:33 +0800

    Add validation for subtraction

--- src/controller/operations/operations.routes.ts
+++ src/controller/operations/operations.routes.ts
@@ -5,7 +5,7 @@ import operationHandler from '@controller/operations/operations.handler';
 const route = Router();
 
 route.post('/addition', ValidateReq('body', 'AdditionOperation'), operationHandler.addition);
-route.post('/subtraction', operationHandler.subtraction);
+route.post('/subtraction', ValidateReq('body', 'SubtractionOperation'), operationHandler.subtraction);
 route.post('/multiplication', operationHandler.multiplication);
 route.post('/division', operationHandler.division);
 
